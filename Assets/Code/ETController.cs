1ba82372f0f32bf02926225868c79f90d1b2b962
gaoziqi <571051761@qq.com>
2019-01-27 13:11:11 +0800

    v1.0.0

--- Assets/Code/ETController.cs
+++ Assets/Code/ETController.cs
@@ -18,14 +18,15 @@ public class ETController : MonoBehaviour
   //获取到场景中的Joystick
   public ETCJoystick moveJoystick;
 
+  //获取场景中的Button
+  public ETCButton jumpButton;
+  public ETCButton jetButton;
+
   public Vector3 debugGro = new Vector3(0.0f, 270.0f, 90.0f);
-  public bool isDebug = false;
 
   private const float lowPassFilterFactor = 0.2f;
-  private Vector3 v = new Vector3(0f, 0f, 0f);
 
-  //获取场景中的Button
-  // public ETCButton controlETCButton;
+
 
   private CharacterController characterController;
   private Transform cameraTransform;
@@ -42,6 +43,17 @@ public class ETController : MonoBehaviour
     this.characterController = this.GetComponent<CharacterController>();
     this.cameraTransform = this.GetComponentInChildren<Camera>().transform;
     this.moveJoystick = ETCInput.GetControlJoystick("MoveJoystick");
+    this.jumpButton.onDown.AddListener(() =>
+    {
+      if (!this.jumpLocked && this.timeInAir < 0.5f)
+      {
+        this.timeInAir = 0.5f;
+        this.verticalSpeed = this.JumpStrength;
+        this.jumpLocked = true;
+      }
+    });
+    this.jumpButton.onUp.AddListener(() => this.jumpLocked = false);
+    this.jetButton.onPressed.AddListener(() => this.verticalSpeed = 2f);
     Cursor.visible = true;
     Cursor.lockState = CursorLockMode.Confined;
     //陀螺仪的设置
@@ -50,12 +62,18 @@ public class ETController : MonoBehaviour
     Input.gyro.updateInterval = 0.01f;
   }
 
+  void Awake()
+  {
+    this.transform.eulerAngles = new Vector3(90, 0, 0);
+  }
+
   void Update()
   {
     bool touchesGround = this.onGround();
     float runMultiplier = 1f + 2f * Input.GetAxis("Run");
     float y = this.transform.position.y;
-    Vector3 movementVector = this.transform.forward * this.moveJoystick.axisY.axisValue + this.transform.right * this.moveJoystick.axisX.axisValue;
+    Vector3 movementVector = this.cameraTransform.forward * this.moveJoystick.axisY.axisValue + this.cameraTransform.right * this.moveJoystick.axisX.axisValue;
+    movementVector.y = 0;
     if (movementVector.sqrMagnitude > 1)
     { // this check prevents partial joystick input from becoming 100% speed
       movementVector.Normalize();  // this prevents diagonal movement form being too fast
@@ -71,10 +89,13 @@ public class ETController : MonoBehaviour
     // this.cameraTransform.localRotation = Quaternion.AngleAxis(this.cameraTilt, Vector3.right);
 
     // this.transform.localRotation = (isDebug ? Quaternion.Euler(debugGro) : Input.gyro.attitude) * new Quaternion(0, 0, 1, 0);
-    this.v = isDebug ? debugGro : (Input.gyro.attitude * new Quaternion(0, 0, 1, 0)).eulerAngles;
+#if UNITY_EDITOR
+    this.cameraTransform.localRotation = Quaternion.Slerp(this.cameraTransform.localRotation, Quaternion.Euler(debugGro), lowPassFilterFactor);
+#else
+    this.cameraTransform.localRotation = Quaternion.Slerp(this.cameraTransform.localRotation, Input.gyro.attitude * new Quaternion(0, 0, 1, 0), lowPassFilterFactor);
+#endif
     // this.transform.localRotation = Quaternion.Euler(new Vector3(0, -this.v.y, 0));
     // this.cameraTransform.localRotation = Quaternion.Euler(new Vector3(90 - this.v.x, 0, 0));
-    this.transform.localRotation = Quaternion.Euler(this.v) * Quaternion.Euler(new Vector3(90f, 90f, 0f));
 
     if (touchesGround)
     {
@@ -93,20 +114,7 @@ public class ETController : MonoBehaviour
     {
       this.verticalSpeed -= 9.18f * Time.deltaTime;
     }
-    if (Input.GetAxisRaw("Jump") < 0.1f)
-    {
-      this.jumpLocked = false;
-    }
-    if (!this.jumpLocked && this.timeInAir < 0.5f && Input.GetAxisRaw("Jump") > 0.1f)
-    {
-      this.timeInAir = 0.5f;
-      this.verticalSpeed = this.JumpStrength;
-      this.jumpLocked = true;
-    }
-    if (Input.GetAxisRaw("Jetpack") > 0.1f)
-    {
-      this.verticalSpeed = 2f;
-    }
+
     this.characterController.Move(Vector3.up * Time.deltaTime * this.verticalSpeed);
 
     if (Input.GetKeyDown(FlightController.OnOffKey))
@@ -120,10 +128,10 @@ public class ETController : MonoBehaviour
     }
   }
 
-  void OnGUI()
-  {
-    GUI.Label(new Rect(50, 100, 500, 20), "Label : " + this.v.x + "       " + this.v.y + "         " + this.v.z);
-  }
+  // void OnGUI()
+  // {
+  //   GUI.Label(new Rect(50, 100, 500, 20), "Label : " + this.v.x + "       " + this.v.y + "         " + this.v.z);
+  // }
 
   public void Enable()
   {
